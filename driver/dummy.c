97127d02c3b0dd416596ddc59dd8748127a2b6ab
yuyayuya <s1t5y3y1@gmail.com>
2023-07-29 07:19:58 +0000

    step2

--- driver/dummy.c
+++ driver/dummy.c
@@ -2,10 +2,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "platform.h"
+
 #include "util.h"
 #include "net.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int
 dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
@@ -13,6 +17,14 @@ dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_
     debugf("dev=%s, type=0x04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
     /* drop data */
+    intr_raise_irq(DUMMY_IRQ);
+    return 0;
+}
+
+static int
+dummy_isr(unsigned int irq, void *id)
+{
+    debugf("irq=%u, dev=%s", irq, ((struct net_device *)id)->name);
     return 0;
 }
 
@@ -39,6 +51,7 @@ dummy_init(void)
         errorf("net_device_register() failure");
         return NULL;
     }
+    intr_request_irq(DUMMY_IRQ, dummy_isr, INTR_IRQ_SHARED, dev->name, dev);
     debugf("initialized, dev=%s", dev->name);
     return dev;
 }
--- net.c
+++ net.c
@@ -105,6 +105,10 @@ net_run(void)
 {
     struct net_device *dev;
 
+    if (intr_run() == -1) {
+        errorf("intr_run() failure");
+        return -1;
+    }
     debugf("open all devices...");
     for (dev = devices; dev; dev = dev->next) {
         net_device_open(dev);
@@ -122,12 +126,17 @@ net_shutdown(void)
     for (dev = devices; dev; dev = dev->next) {
         net_device_close(dev);
     }
+    intr_shutdown();
     debugf("shutting down");
 }
 
 int
 net_init(void)
 {
+    if (intr_init() == -1) {
+        errorf("intr_init() failure");
+        return -1;
+    }
     infof("initialized");
     return 0;
 }
